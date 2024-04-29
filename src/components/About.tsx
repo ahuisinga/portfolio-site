72de41f1eed85e566381b3b71a00dc4e49be00c2
Anna <anna.huisinga@gmail.com>
2024-04-29 11:22:14 -0500

    add about information

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,37 +1,61 @@
+import { ComputerDesktopIcon } from "@heroicons/react/24/outline";
+import { AcademicCapIcon } from "@heroicons/react/24/outline";
+
 export default function AboutComponent() {
+  const techSkills = [
+    "Java",
+    "JavaScript",
+    "Salesforce",
+    "HTML",
+    "CSS",
+    "React",
+    "Python",
+    "Git",
+    "Agile Methodologies",
+    "SQL and NoSQL",
+    "Technical Documentation",
+  ];
   return (
     <div className=" bg-orange-50 px-16 py-4">
       <div className="">
         <h3 className="text-2xl font-bold uppercase text-red-300" id="about">
           About Me
         </h3>
-        <div className="w-56 bg-red-300 py-1"></div>
+        <div className="w-[22rem] bg-red-300 py-1"></div>
       </div>
       <div className="grid grid-cols-1 gap-6 py-3 lg:grid-cols-2">
-        <div className="mx-auto my-auto">
-          <h2 className="text-indigo-300">At A Glance</h2>
-          <p>Education: Master&apos;s degree in Computer Science</p>
-          <p>Skills</p>
-          <ol>
-            <li>JavaScript</li>
-            <li>HTML</li>
-            <li>Java</li>
-            <li>Python</li>
-          </ol>
-          <p>Fun Facts</p>
-          <ol>
-            <li>Roll Tide!</li>
-            <li>Taylor Swift fan</li>
-          </ol>
+        <div className="mt-4 grid grid-cols-2 text-slate-500">
+          <div>
+            <div className="items center flex flex-row space-x-2 text-lg uppercase text-rose-700">
+              <AcademicCapIcon className="h-[1.6rem]" />
+              <p>Education</p>
+            </div>
+            <p>The University of Alabama:</p>
+            <ul className="list-inside list-disc">
+              <li>Master&apos;s degree in CS</li>
+              <li>Bachelor&apos;s degree in CS</li>
+            </ul>
+          </div>
+          <div>
+            <div className="items center flex flex-row space-x-2 text-lg uppercase text-rose-700">
+              <ComputerDesktopIcon className="h-[1.6rem]" />
+              <p>Technical Skills</p>
+            </div>
+            <ul className="list-inside list-disc">
+              {techSkills.sort().map((skill, index) => (
+                <li key={index}>{skill}</li>
+              ))}
+            </ul>
+          </div>
         </div>
         <div className="space-y-4 text-slate-500">
           <p>
             Hi! I&apos;m Anna, a software developer with a knack for crafting
-            innovative solutions and tackling for solving the nittiest and
-            grittiest of challenges. With over three years of experience under
-            my belt, I am currently part of a dynamic team at the forefront of
-            harnessing the power of the Salesforce platform to deliver seamless
-            data solutions to our users.
+            innovative solutions and tackling the nittiest and grittiest of
+            challenges. With over three years of experience under my belt, I am
+            currently part of a dynamic team at the forefront of harnessing the
+            power of the Salesforce platform to deliver seamless data solutions
+            to our users.
           </p>
           <p>
             In the world of Salesforce, I wear many hats. From writing Apex code
