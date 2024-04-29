446172b19c7307e959ebc9085470a47118892243
Anna <anna.huisinga@gmail.com>
2024-04-28 19:41:20 -0500

    base layout for remaining sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import AboutComponent from "@/components/About";
+import ContactComponent from "@/components/Contact";
 import HeroComponent from "@/components/Hero";
-import Image from "next/image";
+import ProjectsComponent from "@/components/Projects";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <HeroComponent />
+      <AboutComponent />
+      <ProjectsComponent />
+      <ContactComponent />
     </main>
   );
 }
