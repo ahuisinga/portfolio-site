56d8039bceac5b02e0ceafed680fcdd8ad705223
Anna <anna.huisinga@gmail.com>
2025-08-22 17:10:42 -0500

    minor bio updates

--- src/components/About/BioFragment.tsx
+++ src/components/About/BioFragment.tsx
@@ -13,8 +13,8 @@ export default function BioFragment() {
         But my favorite part of the job isn&apos;t just the code — it&apos;s the
         people. I love showing off new features to stakeholders, brainstorming
         with teammates, and celebrating those &ldquo;aha!&rdquo; moments
-        together. There&apos;s no fun in writing code if you don&apos;t get to
-        share it.
+        together. Where&apos;s the fun in writing code if you don&apos;t get to
+        share it with someone when it finally works!
       </p>
       <p>
         Outside of feature-building, I mentor newer team members, especially
@@ -29,18 +29,20 @@ export default function BioFragment() {
         above. I earned both my Bachelor&apos;s and Master&apos;s degrees in
         Computer Science from the University of Alabama —{" "}
         <span className="italic">Roll Tide!</span> As you&apos;d guess, I&apos;m
-        a big football fan, but my love for sports goes beyond the gridiron.
-        I&apos;m especially passionate about advocating for women&apos;s sports
-        and leveling the playing field when it comes to media attention. If
-        there&apos;s a game on, I&apos;m probably hosting a watch party.
+        a big football fan, but my love for sports goes beyond Bryant Denny
+        stadium. I&apos;m particularly passionate about advocating for
+        women&apos;s sports and leveling the playing field when it comes to
+        media attention. So if there&apos;s a game on, I&apos;m probably hosting
+        a watch party.
       </p>
       <p>
         Books are another love — epic fantasy, rom-coms, offbeat memoirs — I
-        read it all. I&apos;m always chasing the next great read or swapping
+        read it all. I&apos;m always chasing my next great read or swapping
         theories about my favorite fictional worlds. And when I need a break
         from books and screens, I reach for my craft supplies. It&apos;s
         satisfying to build something tangible after a day of 1s and 0s — at
-        least until the yarn ends up in knots. (Debugging, meet detangling.)
+        least until the yarn ends up in knots. (I really don&apos;t know what is
+        worse — debugging or detangling)
       </p>
       <p>
         Whether I&apos;m diving into a coding challenge, mentoring a teammate,
